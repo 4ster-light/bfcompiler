aee64d7bdfb9ad24d3be7f4df3209bc48c856d86
4ster-light <davidvivarbogonez@gmail.com>
2025-01-26 16:10:26 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::io;
-
 mod compiler;
 mod interpreter;
 mod mode;
 mod repl;
-mod util;
+mod utils;
 
 use compiler::build_bf;
 use interpreter::interpret_bf;
 use mode::Mode;
 use repl::repl_bf;
-use util::{parse_args, read_bf_file};
+use std::io;
+use utils::{parse_args, read_bf_file};
 
 fn main() -> io::Result<()> {
     let (mode, bf_path, save_output) = parse_args();
--- src/repl.rs
+++ src/repl.rs
@@ -4,16 +4,13 @@ use std::io::{self, Write};
 pub fn repl_bf() -> io::Result<()> {
     println!("Brainfuck REPL. Type 'exit' to quit.");
     loop {
+        let mut input = String::new();
         print!("> ");
         io::stdout().flush()?;
-
-        let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-
         if input.trim() == "exit" {
             break;
         }
-
         interpret_bf(input.trim())?;
     }
     Ok(())
